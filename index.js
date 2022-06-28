3f7c219ae0e455fa5ef948bd606da941ed043fda
Eknoor Singh <65605024+Eknoor02@users.noreply.github.com>
2022-06-28 18:53:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const newsdetails = document.getElementById("newsdetails");
 var newsDataArr = [];
 
 // apis 
-const API_KEY = "fd943fec0e7049dd8d10471a71c60425";
+const API_KEY = "fd943fec0e7049dd8d10471a71c60425"; 
 const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=in&apiKey=";
 const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=";
 const BUSINESS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=";
